31628c0fd0ae1c30b6b96789f671537477ebaa5e
feralgoon <joseph.teague@gmail.com>
2018-06-29 09:40:38 -0500

    added default sources to populate database

---
+++ db/startersources.sql
@@ -0,0 +1,8 @@
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (1, 'CNN.com - RSS Channel - HP Hero', 'http://rss.cnn.com/rss/cnn_topstories.rss');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (2, 'Reuters: Science News', 'http://feeds.reuters.com/reuters/scienceNews');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (4, 'USATODAY - Gaming', 'http://rssfeeds.usatoday.com/topgaming');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (6, 'BBC News - World', 'http://feeds.bbci.co.uk/news/world/rss.xml');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (7, 'BBC News - Technology', 'http://feeds.bbci.co.uk/news/technology/rss.xml');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (8, 'Scientific American News', 'http://rss.sciam.com/ScientificAmerican-News');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (9, 'RPG Cast', 'http://feeds.feedburner.com/Rpgcast');
+INSERT INTO `source` (`SourceId`, `SourceName`, `SourceLink`) VALUES (10, 'HLTV.org - The home of competitive Counter-Strike', 'https://www.hltv.org/rss/news');
