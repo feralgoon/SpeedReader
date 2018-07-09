47e095611dbffc4c8453b2f5d2a146914a754695
feralgoon <joseph.teague@gmail.com>
2018-07-09 13:01:17 -0500

    Removed favorite buttons

--- app/controllers/FavoriteController.java
+++ app/controllers/FavoriteController.java
@@ -7,11 +7,11 @@ public class FavoriteController extends Controller
 {
     public Result getAjax(Integer id)
     {
-        return ok("");
+        return ok("gotten");
     }
 
     public Result postAjax(Integer id)
     {
-        return ok("");
+        return ok("posted");
     }
 }
--- public/javascripts/favoritebutton.js
+++ public/javascripts/favoritebutton.js
@@ -1,3 +1,9 @@
 $('.favorite').on('click',function() {
   $(this).toggleClass("fas far");
 });
+
+$('.favbutton').on('click',function() {
+
+    $(this).toggleClass("btn-default btn-success");
+});
+
